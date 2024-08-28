1da93106de06605b5710375a369a8b4e26f57395
HritwikSinghal <HritwikSinghal@users.noreply.github.com>
2024-08-28 12:50:01 +0000

    ✅ Ready to clone and code.

---
+++ music_manager/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for music_manager."""
+
+from music_manager.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from music_manager.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "music_manager"
